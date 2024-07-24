4932afd5782b49abc57116f7a377b383be3f82f4
Xiao Ning <a790554659@gmail.com>
2024-07-24 15:36:54 +0800

    first commit

---
+++ testbasefile.php
@@ -0,0 +1,20 @@
+<?php
+
+class    ExampleClass   {
+    const  CONST_VALUE  =   'value';
+
+    public function exampleMethod($arg1 , $arg2 )
+    {
+       if($arg1> $arg2 ){
+           echo 'arg1 is greater';
+       }else{
+           echo 'arg2 is greater';
+       }
+    }
+}
+
+function example_function() {
+   echo "example function";
+}
+
+?>
